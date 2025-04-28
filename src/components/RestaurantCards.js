f855dcf5f7d309c8dbb492489d3b8894ba59004e
akhil <akhiltadikonda1627@gmail.com>
2025-04-28 16:19:34 -0500

    Fixing the CORS Plugin issue as well as minor changes based on the response data from Swiggy API

--- src/components/RestaurantCards.js
+++ src/components/RestaurantCards.js
@@ -2,14 +2,14 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaurantCards = (props) => {
   const { resData } = props;
-  const { cloudinaryImageId, name, cuisines, deliveryTime, avgRating } =
-    resData?.info;
+  const { cloudinaryImageId, name, cuisines, sla, avgRating } = resData?.info;
+
   return (
     <div className="restaurantcards">
       <img className="card-img" src={CDN_URL + cloudinaryImageId} />
       <h3>{name}</h3>
       <h4>{cuisines.join(" , ")}</h4>
-      <h5>{deliveryTime} Minutes</h5>
+      <h5>{sla?.slaString}</h5>
       <h5>{avgRating}</h5>
     </div>
   );
